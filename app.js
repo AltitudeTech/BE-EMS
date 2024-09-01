0b4b3cd71b7e9c996984eac2e5acab69be9e3659
papasunne <35965929+AltitudeTech@users.noreply.github.com>
2024-09-01 22:48:43 +0100

    update app.js with cron

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
-//import the required dependencies
+// Import the required dependencies
 const express = require("express");
-const db = require("./config/dbConfig");
-require("./models/PatientModel");
+const cron = require("node-cron");
+const axios = require("axios");
 const cors = require("cors");
 const morgan = require("morgan");
 
-//create express app
+// Database configuration
+const db = require("./config/dbConfig");
+
+// Import models
+require("./models/PatientModel");
+
+// Create Express app
 const app = express();
 
-//middleware
+// Middleware setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors({ origin: "*" }));
 app.use(morgan("tiny"));
+
+// Define the port
 const port = process.env.PORT || 5005;
 
-//route
+// Schedule tasks to ping the server every 30 minutes
+cron.schedule("*/30 * * * *", async () => {
+  try {
+    const response = await axios.get(
+      "https://be-ems-production-2721.up.railway.app/"
+    );
+    console.log("Ping successful:", response.status);
+  } catch (error) {
+    console.error("Ping failed:", error.message);
+  }
+});
+
+// Define routes
 app.use("/EMS/patients", require("./routes/patientRoute"));
 app.use("/EMS/staff", require("./routes/StaffDataController"));
 
-const start = () => {
+// Start the server
+const startServer = () => {
   try {
     app.listen(port, () => {
-      console.log(`app is listening to port ${port}`);
+      console.log(`App is listening on port ${port}`);
     });
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error starting the server:", error.message);
+  }
 };
 
-start();
+// Initialize the server
+startServer();
